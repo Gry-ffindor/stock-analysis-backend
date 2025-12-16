a8e29d9a4f38a3d5fb5fc6274cb6e70d478beaa6
Mohit <mohit.kumar@boomgames.in>
2025-12-16 15:29:29 +0530

    Model upgrade

--- agent/agent.py
+++ agent/agent.py
@@ -19,7 +19,7 @@ class AgentState(TypedDict):
 
 
 google_api_key = os.getenv("GOOGLE_API_KEY")
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0, google_api_key=google_api_key)
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0, google_api_key=google_api_key)
 tools = [web_search, money_control_scrap, get_financial_summary]
 
 
